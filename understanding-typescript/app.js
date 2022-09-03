925117e631933120cd9cf72c3f7a1bdf9506eb13
Alex Baxter <alexbaxter5555@gmail.com>
2022-09-02 23:26:47 -0400

    BAX: Video #29

--- understanding-typescript/app.js
+++ understanding-typescript/app.js
@@ -1,15 +1,43 @@
-// Union Types allow different datatypes for any variable if desired
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'number') {
-        result = +input1 + +input2; //Using + in front of a variable casts it to a number datatype
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(5, 4, 'number');
-console.log(combinedAges);
-var combinedNames = combine('Alex', 'Karah', 'number');
-console.log(combinedNames);
+;
+function add2(n1, n2) {
+    return n1 + n2;
+}
+;
+function add3(n1, n2) {
+    return n1.toString() + n2.toString();
+}
+;
+function add4(n1, n2) {
+    return n1.toString() + n2.toString();
+}
+;
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+;
+function printResult2(num) {
+    console.log('Result: ' + num);
+    return;
+}
+;
+var someValue; // this is a value datatype,  
+printResult(add(2, 3));
+// We can also declare pointers to functions by using the "any" type
+var combineValues;
+var combineValues2;
+// But its best to use the "Function" datatype for this purpose
+var combineValues3;
+combineValues = add;
+combineValues3 = add;
+console.log(combineValues(1, 3));
+console.log(combineValues3(1, 9));
+var log = console.log;
+log('Sup everyone');
+function addAndHandle(n1, n2, callback) {
+    var result = n1 + n2;
+    callback(result);
+}
+addAndHandle(10, 20, function () { });
--- understanding-typescript/app.ts
+++ understanding-typescript/app.ts
@@ -1,17 +1,52 @@
-// Union Types allow different datatypes for any variable if desired
-function combine(input1: number | string, input2: number | string, resultConversion: 'number' | 'text') {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'number') {
-        result = +input1 + +input2; //Using + in front of a variable casts it to a number datatype
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1: number, n2: number) {              // inferred return datatype
+    return n1 + n2;
+};
+
+function add2(n1: number, n2: number): number {     // explicit return dataype
+    return n1 + n2;
+};
+
+function add3(n1: number, n2: number) {             // inferred return dataype
+    return n1.toString() + n2.toString();
+};
+
+function add4(n1: number, n2: number): string {     // explicit return dataype
+    return n1.toString() + n2.toString();
+};
+
+function printResult(num: number): void {           // explicit void return type, not needed/not best practice to even include it most of the time
+    console.log('Result: ' + num);
+};
+
+function printResult2(num: number): undefined {      // using undefined as the return type, js EXPECTS a returned value, it just will return "undefined" 
+    console.log('Result: ' + num);
+    return;
+};
+
+let someValue: undefined; // this is a value datatype,  
+
+printResult(add(2, 3));
+
+// We can also declare pointers to functions by using the "any" type
+let combineValues;
+let combineValues2: any;
+// But its best to use the "Function" datatype for this purpose
+let combineValues3: Function;
+
+combineValues = add;
+combineValues3 = add;
+
+console.log(combineValues(1,3));
+console.log(combineValues3(1,9));
+
+let log = console.log;
+
+log('Sup everyone');
+
+// We can create functions which take callbacks or functions as parameters too.
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
 }
 
-const combinedAges = combine(5, 4, 'number');
-console.log(combinedAges);
-
-const combinedNames = combine('Alex', 'Karah', 'number');
-console.log(combinedNames);
+addAndHandle(10, 20, (result) => { log(result); } );
