53a39c3cae3caab09d514d809dd14e8b7ce13632
alex baxter <alexbaxter@carfax.com>
2022-09-23 10:34:56 -0400

    BAX: Video #111

--- understanding-typescript/dist/app.js
+++ understanding-typescript/dist/app.js
@@ -1,12 +1,20 @@
 "use strict";
+//---------------------------------------------------------------------------------------------------------------------------------------
+// Decorators
+//---------------------------------------------------------------------------------------------------------------------------------------
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-// best practice is name decorators with an uppercase starting character, like a class
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+// Decorators should be defined at the top of a file.
+// Best practice is name decorators with an uppercase starting character, like a class
 // Decorators execute when your classes are DEFINED, not when they are initialized/instantiated.
+// Decorators can be used above a lot of places, including classes, interfaces, properties/fields etc...
 // Regular decorator
 function Logger2(constructor) {
     console.log('Logging...');
@@ -14,25 +22,38 @@ function Logger2(constructor) {
 }
 // Decorator factory
 function Logger(logString) {
+    console.log('LOGGER FACTORY');
     return function (constructor) {
         console.log(logString);
         console.log(constructor);
     };
 }
-// Decorator factor #2
+// Decorator factory #2
 function WithTemplate(template, hookId) {
     // We use just an underscore '_' as the parameter name here to tell Typescript 'We know we need an argument, but we aren't going to use it'
     return function (constructor) {
+        console.log('TEMPLATE FACTORY');
         const hookElement = document.getElementById(hookId);
         const person = new constructor();
+        // if the hookElement exists/isn't null
         if (hookElement) {
-            hookElement.innerHTML = template;
-            hookElement.querySelector('h1').textContent = person.name;
+            console.log('Rendering template...');
+            hookElement.innerHTML = template; // Set the html in the DOM to our template string
+            hookElement.querySelector('h1').textContent = person.name; // Query select an h1 element, and set the text to person obj.name
         }
     };
 }
-var Temporary;
-(function (Temporary) {
+//---------------------------------------------------------------------------------------------------------------------------------------
+// Namespace: Decorators
+//---------------------------------------------------------------------------------------------------------------------------------------
+var Decorators;
+(function (Decorators) {
+    //-----------------------------------------------------------------------------------------------------------------------------------
+    // Class: Person
+    //-----------------------------------------------------------------------------------------------------------------------------------
+    // The bottom-most decorator is executed first in there is 2 or more present above a class
+    // This is only true in the case of the ACTUAL decorator function inside of factories
+    // Otherwise, the decorators run top to bottom before they call their return decorator function inside of themselves
     let Person = class Person {
         constructor() {
             this.name = 'Alex';
@@ -45,5 +66,67 @@ var Temporary;
     ], Person);
     const pers = new Person();
     console.log(pers);
-})(Temporary || (Temporary = {}));
+    //-----------------------------------------------------------------------------------------------------------------------------------
+    // Class: Product
+    //-----------------------------------------------------------------------------------------------------------------------------------
+    // Decorator
+    function Log(target, propertyName) {
+        console.log('Property Decorator!');
+        console.log(target, propertyName);
+    }
+    function Log2(target, name, descriptor) {
+        console.log('Accesor Decorator!');
+        console.log('target');
+        console.log(target);
+        console.log('name');
+        console.log(name);
+        console.log('descriptor');
+        console.log(descriptor);
+    }
+    function Log3(target, name, descriptor) {
+        console.log('Method Decorator!');
+        console.log('target');
+        console.log(target);
+        console.log('name');
+        console.log(name);
+        console.log('descriptor');
+        console.log(descriptor);
+    }
+    function Log4(target, name, position) {
+        console.log('Parameter Decorator!');
+        console.log('target');
+        console.log(target);
+        console.log('name');
+        console.log(name);
+        console.log('position');
+        console.log(position);
+    }
+    class Product {
+        constructor(t, price) {
+            this.title = t;
+            this._price = price;
+        }
+        set price(value) {
+            if (value > 0) {
+                this._price = value;
+            }
+            else {
+                throw new Error('Invalid price - value cannot be negative');
+            }
+        }
+        getPriceWithTax(taxRate) {
+            return this._price * (1 + taxRate);
+        }
+    }
+    __decorate([
+        Log
+    ], Product.prototype, "title", void 0);
+    __decorate([
+        Log2
+    ], Product.prototype, "price", null);
+    __decorate([
+        Log3,
+        __param(0, Log4)
+    ], Product.prototype, "getPriceWithTax", null);
+})(Decorators || (Decorators = {}));
 //# sourceMappingURL=app.js.map
--- understanding-typescript/src/app.ts
+++ understanding-typescript/src/app.ts
@@ -1,5 +1,11 @@
-// best practice is name decorators with an uppercase starting character, like a class
+//---------------------------------------------------------------------------------------------------------------------------------------
+// Decorators
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// Decorators should be defined at the top of a file.
+// Best practice is name decorators with an uppercase starting character, like a class
 // Decorators execute when your classes are DEFINED, not when they are initialized/instantiated.
+// Decorators can be used above a lot of places, including classes, interfaces, properties/fields, parameters etc...
 // Regular decorator
 function Logger2(constructor: Function) {
     console.log('Logging...');
@@ -8,6 +14,7 @@ function Logger2(constructor: Function) {
 
 // Decorator factory
 function Logger(logString: string) {
+    console.log('LOGGER FACTORY');
     return function(constructor: Function) {
         console.log(logString);
         console.log(constructor);
@@ -18,18 +25,30 @@ function Logger(logString: string) {
 function WithTemplate(template: string, hookId: string) {
     // We use just an underscore '_' as the parameter name here to tell Typescript 'We know we need an argument, but we aren't going to use it'
     return function(constructor: any) {
+        console.log('TEMPLATE FACTORY');
         const hookElement = document.getElementById(hookId);
         const person = new constructor();
         // if the hookElement exists/isn't null
         if (hookElement) {
+            console.log('Rendering template...')
             hookElement.innerHTML = template;                               // Set the html in the DOM to our template string
             hookElement.querySelector('h1')!.textContent = person.name;     // Query select an h1 element, and set the text to person obj.name
         }
     };
 }
 
-namespace Temporary {
+//---------------------------------------------------------------------------------------------------------------------------------------
+// Namespace: Decorators
+//---------------------------------------------------------------------------------------------------------------------------------------
+namespace Decorators {
 
+    //-----------------------------------------------------------------------------------------------------------------------------------
+    // Class: Person
+    //-----------------------------------------------------------------------------------------------------------------------------------
+    
+    // The bottom-most decorator is executed first in there is 2 or more present above a class
+    // This is only true in the case of the ACTUAL decorator function inside of factories
+    // Otherwise, the decorators run top to bottom before they call their return decorator function inside of themselves
     @Logger('LOGGING - PERSON')
     @WithTemplate('<h1>Person Template</h1>', 'app')
     class Person {
@@ -42,5 +61,71 @@ namespace Temporary {
 
     const pers = new Person();
     console.log(pers);
+
+    //-----------------------------------------------------------------------------------------------------------------------------------
+    // Class: Product
+    //-----------------------------------------------------------------------------------------------------------------------------------
+
+    // Decorator
+    function Log(target: any, propertyName: string | Symbol) {
+        console.log('Property Decorator!');
+        console.log(target, propertyName);
+    }
+
+    function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+        console.log('Accesor Decorator!');
+        console.log('target');
+        console.log(target);
+        console.log('name');
+        console.log(name);
+        console.log('descriptor');
+        console.log(descriptor);
+    }
+
+    function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+        console.log('Method Decorator!');
+        console.log('target');
+        console.log(target);
+        console.log('name');
+        console.log(name);
+        console.log('descriptor');
+        console.log(descriptor);
+    }
+
+    function Log4(target: any, name: string | Symbol, position: number) {
+        console.log('Parameter Decorator!');
+        console.log('target');
+        console.log(target);
+        console.log('name');
+        console.log(name);
+        console.log('position');
+        console.log(position);
+    }
+
+    class Product {
+        @Log
+        title: string;
+        private _price: number;
+
+        @Log2
+        set price(value: number) {
+            if (value > 0) {
+                this._price = value;
+            }
+            else {
+                throw new Error('Invalid price - value cannot be negative');
+            }
+        }
+
+        constructor(t: string, price: number) {
+            this.title = t;
+            this._price = price;
+        }
+
+        @Log3
+        getPriceWithTax(@Log4 taxRate: number) {
+            return this._price * (1 + taxRate);
+        }
+    }
 
 }
