597e916e59c7a43f904cb3df2a3e1ef16becad51
alex baxter <alexbaxter@carfax.com>
2022-09-23 16:20:55 -0400

    BAX: Video #123 - Start

--- drag-drop-project/src/app.ts
+++ drag-drop-project/src/app.ts
@@ -1,1 +1,26 @@
-// First project will be a Monolith 1 file
+// First project will be a large 1 file monolith
+
+namespace DragDropProject {
+
+    class ProjectInput {
+        templateElement: HTMLTemplateElement;
+        hostElement: HTMLDivElement;
+        formElement: HTMLFormElement;
+
+        constructor() {
+            this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+            this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+            const importNode = document.importNode(this.templateElement.content, true);
+            this.formElement = importNode.firstElementChild as HTMLFormElement;
+            this.attach();
+        }
+
+        private attach() {
+            this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
+        }
+    }
+
+    const projectInput = new ProjectInput();
+    console.log(projectInput);
+}
